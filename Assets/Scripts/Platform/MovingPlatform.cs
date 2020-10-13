011317e495271a12a9844b75cb0e4a9c0a4bfd6c
jianghui dai <jianghuidai@outlook.com>
2020-10-12 20:03:12 -0700

    add moving platforms

--- Assets/Scripts/Platform/MovingPlatform.cs
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -1,35 +1,48 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Platform
 {
     public class MovingPlatform : MonoBehaviour
     {
-        private Vector2 velocity;
+        public Vector2 velocity;
+        public bool allowReverse;
         private bool moving;
+        private bool reverse;
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.collider.gameObject.CompareTag("Player"))
             {
                 moving = true;
-                other.collider.transform.SetParent((transform));
+                other.collider.transform.SetParent(transform);
+            }
+            else
+            {
+                reverse = !reverse; 
             }
         }
 
-        private void OnCollisionExit(Collision other)
+        private void OnCollisionExit2D(Collision2D other)
         {
             if (other.collider.gameObject.CompareTag("Player"))
             {
                 other.collider.transform.SetParent(null);
+                moving = false;
             }
         }
 
         private void FixedUpdate()
         {
-            if (moving)
+            if (moving && !reverse)
             {
-                transform.position += (Vector3)(velocity * Time.deltaTime);
+                transform.position += (Vector3) (velocity * Time.deltaTime);
+            }
+            
+            if (moving && allowReverse && reverse)
+            {
+                transform.position += (Vector3) (-velocity * Time.deltaTime);
             }
         }
     }
