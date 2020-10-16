2ab8a8a64570f9e4f7a444449d6935e2417f0248
jianghui dai <jianghuidai@outlook.com>
2020-10-15 17:50:33 -0700

    make respawn animation for the player and the boss, add some sound effects

---
+++ Assets/Scripts/Enemy/ShowBoss.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class ShowBoss : MonoBehaviour
+    {
+        public GameObject boss;
+        public GameObject player;
+
+        private void Awake()
+        {
+            boss.SetActive(true);
+            player.SetActive(true);
+        }
+    }
+}
