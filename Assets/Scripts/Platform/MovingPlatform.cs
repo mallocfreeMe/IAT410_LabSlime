cd41a1507b2303a7f7098b263ce2db8b85618cdc
GoodNat <pengyul@sfu.ca>
2020-10-28 20:18:22 -0700

    fixed the platform bug

--- Assets/Scripts/Platform/MovingPlatform.cs
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -1,28 +1,49 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.Serialization;
 
 namespace Platform
 {
     public class MovingPlatform : MonoBehaviour
     {
-        public Vector2 velocity;
+        public Transform pos1, pos2;
+        public float speed;
+        public Transform startPosition;
+        Vector3 nextPosition;
         private bool reverse;
 
-        private void OnCollisionEnter2D(Collision2D other)
+        void Start()
         {
-            if (other.collider.gameObject.CompareTag("Ground"))
-            {
-                reverse = !reverse;
-            }
+            nextPosition = startPosition.position;
+        }
 
+        void Update()
+        {
+            if (transform.position == pos1.position)
+            {
+                nextPosition = pos2.position;
+            }
+            if (transform.position == pos2.position)
+            {
+                nextPosition = pos1.position;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(pos1.position, pos2.position);
+        }
+        void OnCollisionEnter2D(Collision2D other)
+        {
             if (other.collider.gameObject.CompareTag("Player"))
             {
                 other.collider.transform.SetParent(transform);
             }
         }
 
-        private void OnCollisionExit2D(Collision2D other)
+        void OnCollisionExit2D(Collision2D other)
         {
             if (other.collider.gameObject.CompareTag("Player"))
             {
@@ -30,16 +51,5 @@ namespace Platform
             }
         }
 
-        private void FixedUpdate()
-        {
-            if (!reverse)
-            {
-                transform.position += (Vector3) (velocity * Time.deltaTime);
-            }
-            else
-            {
-                transform.position += (Vector3) (-velocity * Time.deltaTime);
-            }
-        }
     }
 }
