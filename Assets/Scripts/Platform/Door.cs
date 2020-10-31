c8c28ac59da013e7dbfce9787383f72bc996f245
GoodNat <pengyul@sfu.ca>
2020-10-30 18:14:32 -0700

    Change lab level design and level one level design

--- Assets/Scripts/Platform/Door.cs
+++ Assets/Scripts/Platform/Door.cs
@@ -5,9 +5,10 @@ namespace Platform
 {
      public class Door : MonoBehaviour
      {
+        public int sence;
           private void OnCollisionEnter2D(Collision2D other)
           {
-               SceneManager.LoadScene(4);
+               SceneManager.LoadScene(sence);
           }
      }
 }
