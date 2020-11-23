bc769db09e843055b29eef63075fe41928118d20
jda68 <jianghuidai@outlook.com>
2020-11-22 22:20:39 -0800

    update key and gate script

---
+++ Assets/Scripts/Platform/Gate.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace Platform
+{
+    public class Gate : MonoBehaviour
+    {
+        private Animator _animator;
+        public GameObject key;
+        private Key _keyScript;
+        private BoxCollider2D _collider2D;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _keyScript = key.GetComponent<Key>();
+            _collider2D = GetComponent<BoxCollider2D>();
+        }
+
+        private void Update()
+        {
+            if (_keyScript.isPicked)
+            {
+                _animator.SetTrigger("Open");
+                _keyScript.isPicked = false;
+                _collider2D.enabled = false;
+            }
+        }
+    }
+}
