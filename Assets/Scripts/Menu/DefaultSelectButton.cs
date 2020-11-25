0ccff658855876d167ea7993e74033559f590f58
keeponcoding <52342021+garbagecenter@users.noreply.github.com>
2020-11-25 11:30:04 +0800

    pause menu control by keyboard

---
+++ Assets/Scripts/Menu/DefaultSelectButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefaultSelectButton : MonoBehaviour
+{
+
+    public Button selectButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(selectButton != null)
+       {
+           selectButton.Select();    
+           print("selected button");
+       }
+       else 
+       {
+           Debug.Log ("SelectButton was null");
+       }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
